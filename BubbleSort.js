f61b77e8d5486fd7fd6ff8cdb1667ce400cea91a
leyfor <justman51@yahoo.com>
2018-02-18 12:30:06 +0100

    BubbleSort Algorithms

--- BubbleSort.js
+++ BubbleSort.js
@@ -13,7 +13,7 @@ let array = [55, -22, 20, -15, 30, -3, 7];
 for (let lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex-- ) {
     for (let index = 0; index < lastUnsortedIndex; index++) {
         if (array[index] > array[index + 1]) {
-            bubbleSort(array, index, index + 1); 
+            bubbleSort(array, index, index + 1); // Call our bubbleSort function
         }
         
     }
